91c68e68ee6b464c6710c69c22e949e79c5f9e3c
gawdn <gordon.zhong@outlook.com.au>
2019-12-01 21:05:35 +1100

    1-1 star

---
+++ 1-1/1-1.py
@@ -0,0 +1,14 @@
+def calculate_fuel_required(mass):
+    """
+    Calculates the fuel required for a module given it's mass
+    """
+    return (mass//3) - 2
+
+
+if __name__ == "__main__":
+    with open("1-1.in", "r") as masses:
+        total_fuel = 0
+        for mass in masses:
+            total_fuel += calculate_fuel_required(int(mass))
+
+    print(total_fuel)
